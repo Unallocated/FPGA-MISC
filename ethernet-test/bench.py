e87cdd78af727f619f88cca62905f0e3577da9a6
main <main@new-host-4.home>
2015-04-13 00:50:05 -0400

    Script for benchmarking the bandwidth Must run as root!!

---
+++ ethernet-test/bench.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import socket
+import time
+
+s = socket.socket( socket.AF_PACKET , socket.SOCK_RAW , socket.ntohs(0x0003))
+
+startTime = time.time()
+byteCount = 0
+while 1:
+  byteCount += len(s.recvfrom(10000)[0])
+
+  if byteCount >= 1024 * 1024 * 10:
+    print "BW: %0.2f" % (byteCount / int(time.time() - startTime))
+    startTime = time.time()
+    byteCount = 0
+
+
